d6fe442e28e396a03c60fe104a05b123e39c60ed
brazeiro63 <brazeiro63@gmail.com>
2025-05-13 08:18:45 -0300

    Crew para busca de lojas

--- main.py
+++ main.py
@@ -1,15 +1,67 @@
 from mcp.server.fastmcp import FastMCP
-from serper import Serper
+from crewai import Agent, Task, Crew, Process
+from crewai_tools import SerperDevTool, WebsiteSearchTool
+from dotenv import load_dotenv
+from utils.MyLLM import MyLLM
+
+# Carregar variáveis de ambiente
+load_dotenv()
+
+# Definir o modelo de linguagem
+llm = MyLLM.GTP4o_mini
+
+scraper_tool = SerperDevTool()
+scraper_tool.n_results = 20
+web_rag_tool = WebsiteSearchTool()
+
+def main():
 
 
-mcp = FastMCP('MCP Load E-commerce Server - v1.0')
+    # Definindo os Agentes
+    # Defininindo o agente que busca lojas candidatas a afiliação
+    researcher_agent = Agent (
+        role='Affiliate Market Research Analyst',
+        goal='Provide up-to-date affiliate market analysis of best Stores to work with',
+        backstory='An expert analyst with a keen eye for affiliate market trends.',
+        tools=[scraper_tool, web_rag_tool],
+        llm=llm,
+        allow_delegation=False,
+        verbose=True
+    )
 
-#-----------------
-# AFFILIATE STORES
-#-----------------
 
-@mcp.tool(name='find_affiliate_stores')
-def find_affiliate_stores():
-    # Implementation for finding affiliate stores
-    pass
+    # Definindo agente que faz a curadoria das lojas para escolher as melhores
+    curator_agent = Agent(
+        role='Market Analyst',
+        goal='Nail the best choices to get partnership in affiliate market',
+        backstory='A skilled market analyst on treads and forecasting results',
+        llm=llm,
+        allow_delegation=False,
+        verbose=True,
+    )
+
+    # Definindo as Tasks
+    # Task de busca de lojas candidatas
+    research_task = Task(
+        description='Research the best trends in affiliate market in seek of good partner stores',
+        agent='researcher_agent',
+        expected_output='A plaintext list of the markets best choices for partners in affiliate market.'
+    )
+
+
+    # Task de curadoria de lojas
+    selection_task = Task(
+        description='A filtered list with the top 5 Stores to work with in affiliate market.',
+        agent='curator_agent',
+        expected_output='A ordered list of the top 5 companies.'
+    )
+
+    # Define a crew para realização das tarefas
+    store_selction_crew = Crew(
+        agents=[researcher_agent, curator_agent],
+        tasks=[research_task, selection_task],
+        process=Process.sequential,
+    )
+
+    result = store_selction_crew.kickoff(inputs={})
 
---
+++ utils/MyLLM.py
@@ -0,0 +1,21 @@
+from dotenv import load_dotenv
+from crewai import LLM
+import os
+
+load_dotenv()
+
+class MyLLM():
+    GTP4o_mini            = LLM(model='gpt-4o-mini', base_url='https://api.openai.com/v1',api_key=os.getenv('OPENAI_API_KEY'))
+    GPT4o_mini_2024_07_18 = LLM(model='gpt-4o-mini-2024-07-18', base_url='https://api.openai.com/v1',api_key=os.getenv('OPENAI_API_KEY'))
+    GPT_4o_2024_08_06     = LLM(model='gpt-4o-2024-08-06', base_url='https://api.openai.com/v1',api_key=os.getenv('OPENAI_API_KEY'))
+    GTP4o                 = LLM(model='gpt4o', base_url='https://api.openai.com/v1',api_key=os.getenv('OPENAI_API_KEY'))
+    GPT_o1                = LLM(model='o1-preview', base_url='https://api.openai.com/v1',api_key=os.getenv('OPENAI_API_KEY'))
+    GPT_o1_mini           = LLM(model='o1-mini', base_url='https://api.openai.com/v1',api_key=os.getenv('OPENAI_API_KEY'))
+    Ollama_llama_3_1      = LLM(model="ollama/llama3.1", base_url="http://localhost:11434")
+    Claude_3_opus         = LLM(model='claude-3-opus-20240229')
+    LLAMA3_70B            = LLM(model='groq/llama3-70b-8192', base_url='https://api.groq.com/openai/v1', api_key=os.getenv('GROQ_API_KEY'))
+    GROQ_LLAMA            = LLM(model='groq/llama-3.2-3b-preview', base_url='https://api.groq.com/openai/v1', api_key=os.getenv('GROQ_API_KEY'))
+    GROQ_LLAMA2           = LLM(model='groq/llama-3.2-11b-vision-preview', base_url='https://api.groq.com/openai/v1', api_key=os.getenv('GROQ_API_KEY'))
+    GROQ_MIXTRAL          = LLM(model='groq/mixtral-8x7b-32768', base_url='https://api.groq.com/openai/v1', api_key=os.getenv('GROQ_API_KEY'))
+    DEEPSEEK_R1           = LLM(model='deepseek/deepseek-reasoner', base_url='https://api.deepseek.com',api_key=os.getenv('DEEPSEEK_API_KEY'))
+    DEEPSEEK_CHAT         = LLM(model='deepseek/deepseek-chat', base_url='https://api.deepseek.com',api_key=os.getenv('DEEPSEEK_API_KEY'))
